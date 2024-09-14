4e1262a7ec702dc2e4900bb35e2a138864d2e0ce
shirin alizadeh <64709896+shirin-ux@users.noreply.github.com>
2024-09-14 11:38:12 +0330

    Update InsuranceDbContext.cs

--- PremiumAccountService.Infrastructure/Context/InsuranceDbContext.cs
+++ PremiumAccountService.Infrastructure/Context/InsuranceDbContext.cs
@@ -12,6 +12,39 @@ namespace PremiumAccountService.Infrastructure.Context
         public DbSet<Coverage> coverages { get; set; }
         public DbSet<InsuranceRequest> InsuranceRequests { get; set; }
         public DbSet<InsuranceRequestCoverage> InsuranceRequestCoverages { get; set; }
+
+         protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Coverage>().HasData(
+            new Coverage
+            {
+                Id = 1,
+                Type = "جراحی",
+                Rate = 0.0052,
+                MinAmount = 5000,
+                MaxAmount = 50000000
+            },
+            new Coverage
+            {
+                Id = 2,
+                Type = "دندانپزشکی",
+                Rate = 0.0042,
+                MinAmount = 4000,
+                MaxAmount = 40000000
+            },
+            new Coverage
+            {
+                Id = 3,
+                CoverageType = "بستری",
+                Rate = 0.005,
+                MinAmount = 2000,
+                MaxAmount = 20000000
+            }
+        );
+    }
+}
     }
 
 }
